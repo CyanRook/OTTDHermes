2c11742ff05262ca3917fcb0777de0e2b6a9da9b
Ryan Cook <cyanrook@gmail.com>
2013-07-16 12:32:03 -0400

    Added Batch files for my laptop, this might work for your code as well so long as the git is in your documents

---
+++ Hermes_Laptop.bat
@@ -0,0 +1,4 @@
+set source="C:\Users\AsusRook\Documents\OTTDHermes"
+set destination="C:\Program Files\OpenTTD\ai\OTTDHermes"
+xcopy %source% %destination% /y
+call "C:\Users\AsusRook\Documents\OTTDHermes\ottddebug.bat"
