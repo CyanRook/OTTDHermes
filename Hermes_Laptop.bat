f5dd35e98c84675ef0f2cbfb2483ee84b7cbb137
Ryan Cook <cyanrook@gmail.com>
2013-07-18 14:39:54 -0400

    fixed laptop batch file

--- Hermes_Laptop.bat
+++ Hermes_Laptop.bat
@@ -1,4 +1,4 @@
 set source="C:\Users\AsusRook\Documents\OTTDHermes"
 set destination="C:\Program Files\OpenTTD\ai\OTTDHermes"
-xcopy %source% %destination% /y
+xcopy %source% %destination% /y /s
 call "C:\Users\AsusRook\Documents\OTTDHermes\ottddebug.bat"
